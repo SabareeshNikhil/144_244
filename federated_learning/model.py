e986b314f3ea1ff4f35361ae2b7fe5d17f7941b9
Jeff <jeff@lumoshelmet.co>
2019-04-30 19:48:56 -0400

    delete xavier initialization

--- federated_learning/model.py
+++ federated_learning/model.py
@@ -9,14 +9,8 @@ class QNetwork(nn.Module):
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.fc1 = nn.Linear(state_size, fc1_units)
-        nn.init.xavier_uniform_(self.fc1.weight)
-        nn.init.zeros_(self.fc1.bias)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
-        nn.init.xavier_uniform_(self.fc1.weight)
-        nn.init.zeros_(self.fc1.bias)
         self.fc3 = nn.Linear(fc2_units, action_size)
-        nn.init.xavier_uniform_(self.fc1.weight)
-        nn.init.zeros_(self.fc1.bias)
 
     def forward(self, state):
         
